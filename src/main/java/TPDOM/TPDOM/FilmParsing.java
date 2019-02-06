5836fb2a47516271bc3e3c17645fe1528760f9fa
Jesse Ferreira Filho <jessefilhojfnf@gmail.com>
2019-02-07 00:05:02 +0100

    att xpath

--- src/main/java/TPDOM/TPDOM/FilmParsing.java
+++ src/main/java/TPDOM/TPDOM/FilmParsing.java
@@ -1,12 +1,8 @@
 package TPDOM.TPDOM;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,9 +10,15 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class FilmParsing {
@@ -47,7 +49,6 @@ public class FilmParsing {
 		System.out.println(" ");
 		System.out.println(" ");
 		System.out.println("---------- START ----------");
-		System.out.println(root.getElementsByTagName("film").item(1).getChildNodes().getLength());
 		userInteration(root);
 		System.out.println("finish...");
 
@@ -58,6 +59,7 @@ public class FilmParsing {
 		System.out.println("Enter with your choose: ");
 		System.out.println("1 - List of actors;");
 		System.out.println("2 - Return dates;");
+		System.out.println("3 - xPath;");
 		System.out.println("99 - To exit;");
 
 		Scanner scanner = new Scanner(System.in);
@@ -69,6 +71,14 @@ public class FilmParsing {
 			userInteration(root);
 		case 2:
 			datesRetour(root);
+			userInteration(root);
+		case 3:
+			try {
+				xPathFunction(root);
+			} catch (XPathExpressionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			userInteration(root);
 
 		case 99:
@@ -108,20 +118,73 @@ public class FilmParsing {
 
 		for (int i = 1; i <= children.getLength(); i++) {
 			System.out.println("");
-			
+
 			try {
 				if(root.getElementsByTagName("rent").item(i).getAttributes().getNamedItem("date") != null) {
 					System.out.println("DVD: " + root.getElementsByTagName("title").item(i).getTextContent());
 					System.out.println("return at: " +root.getElementsByTagName("rent").item(i).getAttributes().getNamedItem("date"));
 				}
-				} catch (NullPointerException e) {
+			} catch (NullPointerException e) {
 				e.getStackTrace();
 			}
 
-
-
 		}
 
+	}
+	private static void xPathFunction(Element root) throws XPathExpressionException{
+		XPathFactory xpathFactory = XPathFactory.newInstance();
+		XPath path = xpathFactory.newXPath();
+
+		String expressionTitles = "//film/title";
+		String expressionRent = "//DVD";
+		NodeList nlistOfTitles = null;
+		NodeList nlistOfRents = null;
+		
+		try {
+			//List of titles
+			System.out.println("List of titles with xPath:");
+			nlistOfTitles = (NodeList)path.evaluate(expressionTitles, root,XPathConstants.NODESET);
+			for (int i = 0; i < nlistOfTitles.getLength(); i++) {
+				System.out.println(" -> "+nlistOfTitles.item(i).getTextContent()+";");
+			}
+			System.out.println("-------------------------------------");
+			//List of titles
+			System.out.println("List of films rented:");
+			nlistOfRents = (NodeList)path.evaluate(expressionRent, root,XPathConstants.NODESET);
+			nlistOfTitles = (NodeList)path.evaluate(expressionTitles, root,XPathConstants.NODESET);
+			for (int i = 0; i < nlistOfTitles.getLength(); i++) {			
+				if(nlistOfRents.item(i).getChildNodes().item(3) != null) {
+					System.out.println(" -> "+nlistOfTitles.item(i).getTextContent()+";");				
+				}
+			}
+			System.out.println("-------------------------------------");
+			//List of qty actors by film
+			System.out.println("List of qty actors by film:");
+
+			for (int i = 1; i <= nlistOfTitles.getLength(); i++) {
+				XPathExpression expr1 = path.compile("//DVD["+i+"]/film/title/text()");
+				String n1_movie = (String)expr1.evaluate(root,XPathConstants.STRING);
+				System.out.print(n1_movie+ " has ");
+				int count = 0;
+				Node no = (Node)path.evaluate("//DVD["+i+"]/film", root,XPathConstants.NODE);
+
+				for (int j = 1; j < no.getChildNodes().getLength(); j++) {
+					if (j%2 != 0) {
+						if (no.getChildNodes().item(j).getNodeName().equals("actor")) {
+							count++;
+						}
+					}
+				}
+				System.out.println(count+ " actors.");
+
+			}
+
+
+		} catch (XPathExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//System.out.println(str);
 
 
 	}
